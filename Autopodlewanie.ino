13df04a8f7187e545ecf3bfe30a9d24e30c1b88e
Bl4nk44977 <paulsonb527@gmail.com>
2025-07-21 13:21:31 +0200

    Update Autopodlewanie.ino
    changed language of my comments to be more accesible for people not understanding Polish :), have fun reading

--- Autopodlewanie.ino
+++ Autopodlewanie.ino
@@ -7,48 +7,36 @@
 #define kwiatek2 12
 #define kwiatek3 11
 #define kwiatek4 10
-// #define dioda1 2
-// #define dioda2 3
-// #define dioda3 4
-// #define dioda4 5
 bool everyOther = true;
+
 void setup() {
   pinMode(kwiatek1, OUTPUT);
   pinMode(kwiatek2, OUTPUT);
   pinMode(kwiatek3, OUTPUT);
   pinMode(kwiatek4, OUTPUT);
-  // pinMode(dioda1, OUTPUT);
-  // pinMode(dioda2, OUTPUT);
-  // pinMode(dioda3, OUTPUT);
-  // pinMode(dioda4, OUTPUT);
 }
 
-// długa doniczka - 24 sekundy
-// mała - 8,5 sekund (test)
-// 1 sekunda = 50 ml na pompce 1 i pompce 2
-// 1 sekunda = 70ml na pompce 3 i pompce 4
-
+// long pot - 24s
+// small pot - 8,5s (test)
+// 1s = 50 ml on kwiatek1 i kwatek2
+// 1s = 70ml on kwiatek3 i kwiatek4
+// in this case I have two long pots and two small ones, but this is easily editable, feel free to change delays to your needs.
 void loop() {
   digitalWrite(kwiatek1, HIGH);
-  // digitalWrite(dioda1, HIGH);
   delay(24000);
   digitalWrite(kwiatek1, LOW);
   digitalWrite(kwiatek2, HIGH);
-  // digitalWrite(dioda2, HIGH);
-  // digitalWrite(dioda1, LOW);
   delay(24000);
   digitalWrite(kwiatek2, LOW);
-
+// in this case my long pots have to be watered twice a week, instead of once so I implemented this part of code below to do so.
   if (everyOther) {
     digitalWrite(kwiatek3, HIGH);
     delay(8500);
     digitalWrite(kwiatek3, LOW);
     digitalWrite(kwiatek4, HIGH);
-    // digitalWrite(dioda4, HIGH);
-    // digitalWrite(dioda3, LOW);
     delay(8500);
     digitalWrite(kwiatek4, LOW);
   }
   everyOther = !everyOther;
-  delay(345600000); //czekaj 4 dni
+  delay(345600000); //wait 4 days 
 }
